6bde586e5a90c5a96283f3a5a7df7671995a9076
Gihyeon <hamzzi99@naver.com>
2025-09-20 06:13:21 +0900

    싹 다 밀어
    언리얼5.6 뭐이리 많이 바뀌었어;

--- Source/InventoryProject/InventoryProject.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "InventoryProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InventoryProject, "InventoryProject" );
-
-DEFINE_LOG_CATEGORY(LogInventoryProject)
