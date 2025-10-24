51652d9509e85538a4e0b888c30e0611d0a9c139
Gihyeon <hamzzi99@naver.com>
2025-10-24 16:22:48 +0900

    라인트레이스 백업
    아직 다 완료하지 못한 파일들입니다.

--- Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -1,8 +1,17 @@
 #include "Player/Inv_PlayerController.h"
 
+#include "Kismet/GameplayStatics.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "Widgets/HUD/Inv_HUDWidget.h"
+
+AInv_PlayerController::AInv_PlayerController()
+{
+	// 추적길이 초기화
+	PrimaryActorTick.bCanEverTick = true;
+	TraceLength = 500.0;
+}
+
 void AInv_PlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -41,4 +50,24 @@ void AInv_PlayerController::CreateHUDWidget() // 위젯 생성 부분
 	{
 		HUDWidget->AddToViewport();
 	}
+}
+
+void AInv_PlayerController::TraceForItem()
+{
+	// 바라보는 뷰포트 좌표 반환
+	if (!IsValid(GEngine) || !IsValid(GEngine->GameViewport)) return;
+	FVector2D ViewportSize;
+	GEngine->GameViewport->GetViewportSize(ViewportSize);
+	const FVector2D ViewportCenter = ViewportSize / 2.f; // 뷰포트 중앙 좌표
+
+	FVector TraceStart;
+	FVector Forward; //당연히 법선벡터
+	if (!UGameplayStatics::DeprojectScreenToWorld(this, ViewportCenter, TraceStart, Forward)) return;
+
+	const FVector TraceEnd = TraceStart + (Forward * TraceLength); //괄호는 쉽게 변경할 수 있는 매개변수	
+	FHitResult HitResult;
+	//선 추적부분 기억하지 이제 매 프레임마다 라인트레이스를 한다. (ToonTanks 처럼)
+	GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ItemTraceChannel);
+
+
 }
--- Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
+++ Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -15,6 +15,8 @@ UCLASS()
 class INVENTORY_API AInv_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AInv_PlayerController();
 protected:
 	virtual void BeginPlay() override;
 	virtual void SetupInputComponent() override;
@@ -23,6 +25,8 @@ private:
 
 	void PrimaryInteract();
 	void CreateHUDWidget(); // 위젯 생성 함수 선언
+	void TraceForItem(); // 아이템 등록
+	
 
 	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
 	TArray<TObjectPtr<UInputMappingContext>> DefaultIMCs; // TArray로부터 단일 포인터를 배열화 시켜가지고 여러개 복수 포인터로 만들 수 있다! 으하하
@@ -35,4 +39,10 @@ private:
 
 	UPROPERTY()
 	TObjectPtr<UInv_HUDWidget> HUDWidget; // 위젯 인스턴스
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")	
+	double TraceLength; // 추적 길이
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")	
+	TEnumAsByte<ECollisionChannel> ItemTraceChannel; // 추적 채널? 충동 채널?
 };
