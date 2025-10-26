59464268f7786f188702df70d6878aeb1803742c
Gihyeon <hamzzi99@naver.com>
2025-10-27 03:54:13 +0900

    인벤토리 4가지 창
    어 일단 만들었습니다

--- Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_SpatialInventory.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_SpatialInventory.cpp
@@ -3,3 +3,55 @@
 
 #include "Widgets/Inventory/Spatial/Inv_SpatialInventory.h"
 
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+#include "Widgets/Inventory/Spatial/Inv_InventoryGrid.h"
+
+void UInv_SpatialInventory::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	Button_Equippables->OnClicked.AddDynamic(this, &ThisClass::ShowEquippables);
+	Button_Consumables->OnClicked.AddDynamic(this, &ThisClass::ShowConsumables);
+	Button_Craftables->OnClicked.AddDynamic(this, &ThisClass::ShowCraftables);
+	Button_Builds->OnClicked.AddDynamic(this, &ThisClass::ShowBuilds); // 빌드 부분 내가 만든 것.
+
+	ShowEquippables(); // 기본값으로 장비창을 보여주자.
+}
+
+void UInv_SpatialInventory::ShowEquippables()
+{
+	SetActiveGrid(Grid_Equippables, Button_Equippables);
+}
+
+void UInv_SpatialInventory::ShowConsumables()
+{
+	SetActiveGrid(Grid_Consumables, Button_Consumables);
+}
+
+void UInv_SpatialInventory::ShowCraftables()
+{
+	SetActiveGrid(Grid_Craftables, Button_Craftables);
+}
+
+void UInv_SpatialInventory::ShowBuilds()
+{
+	SetActiveGrid(Grid_Builds, Button_Builds);
+}
+
+//리펙토링을 이렇게 하네 신기하다.
+void UInv_SpatialInventory::DisableButton(UButton* Button)
+{
+	Button_Equippables->SetIsEnabled(true);
+	Button_Consumables->SetIsEnabled(true);
+	Button_Craftables->SetIsEnabled(true);
+	Button_Builds->SetIsEnabled(true);
+	Button->SetIsEnabled(false);
+}
+
+void UInv_SpatialInventory::SetActiveGrid(UInv_InventoryGrid* Grid, UButton* Button)
+{
+	DisableButton(Button);
+
+	Switcher->SetActiveWidget(Grid);
+}
--- Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
@@ -8,6 +8,7 @@
 
 class UInv_InventoryGrid;
 class UWidgetSwitcher;
+class UButton;
 
 /**
  * 
@@ -17,9 +18,15 @@ class INVENTORY_API UInv_SpatialInventory : public UInv_InventoryBase
 {
 	GENERATED_BODY()
 	
-private:
+public:
+	virtual void NativeOnInitialized() override; // NativeOnInitialized 이게 뭐지?
+
+private: // 여기 있는 UPROPERTY와 위젯과의 이름이 동일해야만함.
+	//스위치
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UWidgetSwitcher> Switcher;
+
+	//메뉴 등록
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UInv_InventoryGrid> Grid_Equippables;
 	UPROPERTY(meta = (BindWidget))
@@ -28,4 +35,35 @@ private:
 	TObjectPtr<UInv_InventoryGrid> Grid_Craftables;
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UInv_InventoryGrid> Grid_Builds;
+
+
+	//버튼 등록
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Equippables;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Consumables;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Craftables;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Builds;
+
+	UFUNCTION()
+	void ShowEquippables();
+
+	UFUNCTION()
+	void ShowConsumables();
+
+	UFUNCTION()
+	void ShowCraftables();
+
+	UFUNCTION()
+	void ShowBuilds();
+
+	void DisableButton(UButton* Button);
+
+	void SetActiveGrid(UInv_InventoryGrid* Grid, UButton* Button);
+
 };
