d11ee23f5808f96dfff40f99dbf318a2ff7498b7
Gihyeon <hamzzi99@naver.com>
2025-10-31 15:25:41 +0900

    아이템 서버에 전달
    Manifest와 게임플레이태그 연동

--- Plugins/Inventory/Source/Inventory/Private/Items/Inv_ItemTags.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Items/Inv_ItemTags.cpp
@@ -2,21 +2,55 @@
 
 namespace GameItems
 {
-	namespace Equipment
+	namespace Equipment // 장비
 	{
 		namespace Weapons
 		{
-			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Axe, "GameItems.Equipment.Weapon.Axe", "Axe")
+			UE_DEFINE_GAMEPLAY_TAG(Axe, "GameItems.Equipment.Weapon.Axe")
+			UE_DEFINE_GAMEPLAY_TAG(Sword, "GameItems.Equipment.Weapon.Sword")
 		}
 
 		namespace Cloaks
 		{
-
+			UE_DEFINE_GAMEPLAY_TAG(RedCloak, "GameItems.Equipment.Cloaks.Redcloak")
 		}
 		namespace Masks
 		{
-
+			UE_DEFINE_GAMEPLAY_TAG(SteelMask, "GameItems.Equipment.Cloaks.SteelMask")
 		}
 
 	}
+
+	namespace Consumables
+	{
+		namespace Potions // 포션
+		{
+			namespace Red
+			{
+				UE_DEFINE_GAMEPLAY_TAG(Small, "GameItems.Consumables.Potions.Red.Small")
+				UE_DEFINE_GAMEPLAY_TAG(Large, "GameItems.Consumables.Potions.Red.Large")
+			}
+
+			namespace Blue
+			{
+				UE_DEFINE_GAMEPLAY_TAG(Small, "GameItems.Consumables.Potions.Blue.Small")
+				UE_DEFINE_GAMEPLAY_TAG(Large, "GameItems.Consumables.Potions.Blue.Large")
+			}
+		}
+	}
+
+	namespace Craftables // 재료
+	{
+		UE_DEFINE_GAMEPLAY_TAG(FireFernFruit, "GameItems.Craftables.Blue.FireFernFruit")
+		UE_DEFINE_GAMEPLAY_TAG(LuminDaisy, "GameItems.Craftables.Blue.LuminDaisy")
+		UE_DEFINE_GAMEPLAY_TAG(ScorchPetalBlossom, "GameItems.Craftables.Blue.ScorchPetalBlossom")
+	}
+
+	namespace Builds // 건축
+	{
+
+	}
 }
+
+
+//GameItems::Equipment::Weapons::Axe // 식별자에 접근법.
--- Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
+++ Plugins/Inventory/Source/Inventory/Public/Items/Manifest/Inv_ItemManifest.h
@@ -2,6 +2,7 @@
 #include "CoreMinimal.h"
 #include "Types/Inv_GridTypes.h"
 #include "StructUtils/InstancedStruct.h"
+#include "GameplayTagContainer.h"
 
 #include "Inv_ItemManifest.generated.h"
 
@@ -17,10 +18,17 @@ struct INVENTORY_API FInv_ItemManifest
 	GENERATED_BODY()
 
 	UInv_InventoryItem* Manifest(UObject* NewOuter); //새로운 인벤토리 아이템 만들 때?
-	EInv_ItemCategory GetItemCategory() const { return ItemCategory; }
+	EInv_ItemCategory GetItemCategory() const { return ItemCategory; } // 아이템 카테고리 얻기
+	FGameplayTag GetItemType() const { return ItemType; } // 아이템 타입 얻기
 
 private:
 	UPROPERTY(EditAnywhere, Category = "Inventory")
 	EInv_ItemCategory ItemCategory{ EInv_ItemCategory::None }; // 개별 구성요소?
+	
+
+	// 게임플레이 태그 부분
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	FGameplayTag ItemType;
+
 };
 
