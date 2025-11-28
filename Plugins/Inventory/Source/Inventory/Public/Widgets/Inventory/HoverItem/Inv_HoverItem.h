a50480c6891b782c6ab3498f1d496f948fbc5e14
Gihyeon <hamzzi99@naver.com>
2025-11-28 16:22:58 +0900

    GridSlot 위젯 옮기기 위한 함수들 선언
    참조하자 참조하자

---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/HoverItem/Inv_HoverItem.h
@@ -0,0 +1,51 @@
+// Gihyeon's Inventory Project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Blueprint/UserWidget.h"
+#include "Inv_HoverItem.generated.h"
+
+/**
+ * GridSlot 내에서 아이템을 마우스로 쭈욱 끌어당겨 테트리스를 할 수 있어요!
+ */
+class UInv_InventoryItem;
+class UImage;
+class UTextBlock;
+
+UCLASS()
+class INVENTORY_API UInv_HoverItem : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	void SetImageBrush(const FSlateBrush& Brush) const; //슬레이트 브러시 설정 함수
+	void UpdateStackCount(const int32 Count) const;
+
+	//참조를 하기 위한 그리드에서 기존 얻던 것들. (오로지 이동하기 위해)
+	FGameplayTag GetItemType() const;
+	int32 GetStackCount() const { return StackCount; }
+	bool IsStackable() const { return bIsStackable; }
+	void SetIsStackable(bool bStacks);
+	int32 GetPreviousGridIndex() const { return PreviousGridIndex; }
+	void SetPreviousGridIndex(int32 Index) { PreviousGridIndex = Index; }
+	FIntPoint GetGridDimensions() const { return GridDimensions; }
+	void SetGridDimensions(const FIntPoint Dimensions) { GridDimensions = Dimensions; }
+	UInv_InventoryItem* GetInventoryItem() const;
+	void SetInventoryItem(UInv_InventoryItem* Item);
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_Icon;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> Text_StackCount;
+
+	//이전 그리드 인덱스를 알아내는 변수 우리가 Grid에서 알아내야 할 변수들
+	int32 PreviousGridIndex;
+	FIntPoint GridDimensions;
+	TWeakObjectPtr<UInv_InventoryItem> InventoryItem;
+	bool bIsStackable{ false };
+	int32 StackCount{ 0 };
+
+};
