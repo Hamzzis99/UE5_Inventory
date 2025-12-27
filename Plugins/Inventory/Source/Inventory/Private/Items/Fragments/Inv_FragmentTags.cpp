6094dbc2abb11a566e4a8d66257e78ced3cfbe36
Gihyeon <hamzzi99@naver.com>
2025-12-28 03:52:50 +0900

    아이템 설명 태그창 만들기
    아이템 디테일하게 보는 부분들은 끝!

--- Plugins/Inventory/Source/Inventory/Private/Items/Fragments/Inv_FragmentTags.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Items/Fragments/Inv_FragmentTags.cpp
@@ -6,9 +6,14 @@ namespace FragmentTags
 	UE_DEFINE_GAMEPLAY_TAG(IconFragment, "FragmentTags.IconFragment")
 	UE_DEFINE_GAMEPLAY_TAG(StackableFragment, "FragmentTags.StackableFragment")
 	UE_DEFINE_GAMEPLAY_TAG(ConsumableFragment, "FragmentTags.ConsumableFragment")
-	UE_DEFINE_GAMEPLAY_TAG(ItemNameFragment, "FragmentTags.ItemNameFragment")
 	
+	UE_DEFINE_GAMEPLAY_TAG(ItemNameFragment, "FragmentTags.ItemNameFragment")
 	UE_DEFINE_GAMEPLAY_TAG(PrimaryStatFragment, "FragmentTags.PrimaryStatFragment")
+	
+	UE_DEFINE_GAMEPLAY_TAG(ItemTypeFragment, "FragmentTags.ItemTypeFragment")
+	UE_DEFINE_GAMEPLAY_TAG(FlavorTextFragment, "FragmentTags.FlavorTextFragment")
+	UE_DEFINE_GAMEPLAY_TAG(SellValueFragment, "FragmentTags.SellValueFragment")
+	UE_DEFINE_GAMEPLAY_TAG(RequiredLevelFragment, "FragmentTags.RequiredLevelFragment")
 	
 	namespace StatMod // 아이템 능력치를 변경해주는 역할.
 	{
