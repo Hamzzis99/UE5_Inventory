b0a1926355d9f8daafed09eeb8341ac18d325894
Gihyeon <hamzzi99@naver.com>
2025-12-28 09:53:33 +0900

    게임플레이태그 고침.
    언리얼이 문제라 안 뜬거네

--- Plugins/Inventory/Source/Inventory/Public/Items/Fragments/Inv_FragmentTags.h
+++ Plugins/Inventory/Source/Inventory/Public/Items/Fragments/Inv_FragmentTags.h
@@ -8,20 +8,19 @@ namespace FragmentTags
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(IconFragment)
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(StackableFragment)
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(ConsumableFragment)
-	
+
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(ItemNameFragment)
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(PrimaryStatFragment)
-	
-	// 디테일한 단순 텍스트 설명 부분들
+
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(ItemTypeFragment)
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(FlavorTextFragment)
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(SellValueFragment)
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(RequiredLevelFragment)	
-	
-	namespace StatMod // 아이템 능력치를 변경해주는 역할.
+
+	namespace StatMod
 	{
-		UE_DECLARE_GAMEPLAY_TAG_EXTERN(StatMod_1)	
-		UE_DECLARE_GAMEPLAY_TAG_EXTERN(StatMod_2)	
-		UE_DECLARE_GAMEPLAY_TAG_EXTERN(StatMod_3)	
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(StatMod_1)
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(StatMod_2)
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(StatMod_3)
 	}
 }
